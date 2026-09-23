a492141cc8abae97447fa6e3e10183ea56447be9
ASTROLOG5204519 <bistroffdj@gmail.com>
2026-09-23 05:26:43 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE='vitaly-bystrov-v6';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='vitaly-bystrov-v6').map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const x=res.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return res}).catch(()=>caches.match('./index.html'))))});
+const CACHE='vitaly-bystrov-v8';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.add('./'))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
