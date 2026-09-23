2046d12743bf582a4bd8b660d0e85fb48a1a5101
ASTROLOG5204519 <bistroffdj@gmail.com>
2026-09-23 15:07:30 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vitaly-bystrov-v8';
+const CACHE='vitaly-bystrov-v8-2';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.add('./'))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
